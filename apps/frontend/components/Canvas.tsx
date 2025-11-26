38320d1085951b34c5ef211da8d83160afeaab3f
Satvik <codesatvik@gmail.com>
2025-11-26 23:43:32 +0530

    added topbar iconbutton selection ,scroll restriction

--- apps/frontend/components/Canvas.tsx
+++ apps/frontend/components/Canvas.tsx
@@ -1,5 +1,9 @@
 import { initDraw } from "@/draw";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { IconButton } from "./IconButton";
+import { Circle, Pencil, RectangleHorizontalIcon } from "lucide-react";
+
+type Shape = "circle" | "rect" | "pencil"
 
 export function Canvas({
     roomId,
@@ -9,12 +13,35 @@ export function Canvas({
     roomId: string;
 }) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [selectedTool, setSelectedTool] = useState<Shape>("circle")
     useEffect(() => {
         if (canvasRef.current) {
             initDraw(canvasRef.current, roomId, socket);
         }
     }, [canvasRef]);
-    return <div>
-     <canvas ref={canvasRef} width={2000} height={1000}></canvas>
+    return <div style={{
+        height: "100vh",
+        overflow:"hidden"
+     }}> 
+        <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}></canvas>
+        <Topbar setSelectedTool={setSelectedTool} selectedTool={selectedTool}></Topbar>
+    </div>
+}
+
+function Topbar({ selectedTool, setSelectedTool}: {
+    selectedTool: Shape,
+    setSelectedTool: (s: Shape) => void
+}) {
+    return<div style={{
+            position: "fixed",
+            top: 10,
+            left:10,  
+    }}>
+        
+        <div className="flex gap-2">
+            <IconButton onClick={() => {setSelectedTool("pencil")}}activated={selectedTool === "pencil"} icon={<Pencil />}  ></IconButton>
+         <IconButton onClick={() => {setSelectedTool("rect")}} activated={selectedTool === "rect"} icon={<RectangleHorizontalIcon />}></IconButton>
+         <IconButton onClick={() => {setSelectedTool("circle")}} activated={selectedTool === "circle"} icon={<Circle />} ></IconButton>
+        </div>
     </div>
 }
